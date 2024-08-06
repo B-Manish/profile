e1711030fba05f5225aa9a79edca64f6be511f10
B-Manish <batchumanish@gmail.com>
2024-08-07 00:01:39 +0530

    Added about me

---
+++ src/components/Aboutme.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Box } from "@mui/material";
+import "../App.css";
+
+function Aboutme() {
+  return (
+    <Box sx={{ width: "900px", height: "500px", border: "1px solid red" }}>
+      <Box
+        sx={{ display: "flex", border: "1px solid grey", mb: "37px" }}
+        className="roboto"
+      >
+        <Box sx={{ color: "#5BF2CE", fontSize: "24px", pt: "7px" }}>01.</Box>
+        <Box
+          className="customdmsans"
+          sx={{ color: "#A7C3E5", fontWeight: "600", fontSize: "32px" }}
+        >
+          About me
+        </Box>
+      </Box>
+      <Box sx={{ display: "flex" }}>
+        <Box sx={{ width: "511px" }}>
+          <Box className="customdmsans" sx={{ fontWeight: "100" }}>
+            Hello! My name is Brittany and I enjoy creating things that live on
+            the internet. My interest in web development started back in 2012
+            when I decided to try editing custom Tumblr themes — turns out
+            hacking together a custom reblog button taught me a lot about HTML &
+            CSS!
+          </Box>
+        </Box>
+        <Box sx={{ width: "389px" }}>r</Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default Aboutme;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,57 @@
 import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 import "../App.css";
-
+import Aboutme from "./Aboutme";
 function Home() {
   return (
-    <Box
-      sx={{
-        border: "1px solid yellow",
-        height: "100%",
-        display: "grid",
-        placeItems: "center",
-        border: "1px solid red",
-      }}
-    >
-      <Box>
-        <Box className="roboto" sx={{ color: "#5BF2CE" }}>
-          Hi, my name is
-        </Box>
-        <Box
-          className="customdmsans"
-          sx={{ color: "#A7C3E5", fontSize: "80px", fontWeight: "600" }}
-        >
-          Manish Batchu
-        </Box>
-        <Box
-          className="customdmsans"
-          sx={{ color: "#A19A8F", fontSize: "80px", fontWeight: "600" }}
-        >
-          I build things for the web
-        </Box>
+    <>
+      <Box
+        sx={{
+          border: "1px solid yellow",
+          height: "100%",
+          display: "grid",
+          placeItems: "center",
+          border: "1px solid red",
+        }}
+      >
         <Box>
-          I’m a software engineer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
-          building accessible, human-centered products at Upstatement.
+          <Box className="roboto" sx={{ color: "#5BF2CE" }}>
+            Hi, my name is
+          </Box>
+          <Box
+            className="customdmsans"
+            sx={{
+              color: "#A7C3E5",
+              fontSize: "80px",
+              fontWeight: "600",
+            }}
+          >
+            Manish Batchu
+          </Box>
+          <Box
+            className="customdmsans"
+            sx={{ color: "#A19A8F", fontSize: "80px", fontWeight: "600" }}
+          >
+            I build things for the web
+          </Box>
+          <Box>
+            I’m a software engineer specializing in building (and occasionally
+            designing) exceptional digital experiences. Currently, I’m focused
+            on building accessible, human-centered products at Upstatement.
+          </Box>
         </Box>
       </Box>
-    </Box>
+      <Box
+        sx={{
+          border: "1px solid yellow",
+          display: "grid",
+          justifyContent: "center",
+          background: "#0F172A",
+        }}
+      >
+        <Aboutme />
+      </Box>
+    </>
   );
 }
 
