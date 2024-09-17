98c57f015b8d231e7339c56b27c62ab8b90f3907
B-Manish <batchumanish@gmail.com>
2024-09-18 00:47:52 +0530

    added fixed info in template

--- src/components/Home.js
+++ src/components/Home.js
@@ -55,7 +55,7 @@ function Home() {
         <Built />
         <Getintouch />
       </Box>
-      <Threed />
+      {/* <Threed /> */}
     </>
   );
 }
--- src/components/Template.js
+++ src/components/Template.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box, Grid } from "@mui/material";
 import Navbar from "./Navbar";
+import "../App.css";
 
 function Template({ page }) {
   return (
@@ -11,6 +12,34 @@ function Template({ page }) {
         background: "#081324",
       }}
     >
+      <Box
+        sx={{
+          position: "fixed",
+          bottom: "100px",
+          left: "100px",
+          height: "50px",
+          width: "50px",
+          backgroundColor: "blue",
+        }}
+      />
+      <Box
+        sx={{
+          position: "fixed",
+          bottom: "500px",
+          right: "100px",
+          height: "50px",
+          width: "50px",
+          border: "1px solid red",
+          color: "#A8B2D1",
+          fontSize: "12px",
+          rotate: "90deg",
+        }}
+        className="roboto"
+      >
+        <Box> bmanish7777@gmail.com</Box>
+        <Box sx={{ height: "30px", width: "10px", background: "grey" }} />
+      </Box>
+
       <Navbar />
       <Box
         sx={{
