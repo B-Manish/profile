0d9518913b82e9d59ce2df3ca53b0b62d70592c7
B-Manish <batchumanish@gmail.com>
2024-10-18 19:21:27 +0530

    Navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 import "../App.css";
 import Logo from "../static/logo.png";
@@ -19,9 +19,54 @@ function Navbar() {
     gsap.fromTo(".logo", { opacity: 0 }, { opacity: 1, delay: "0.2" });
   }, []);
 
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(false); // To prevent multiple GSAP calls
+  const [isAtTop, setIsAtTop] = useState(true); // New state to track if at top
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPosition = window.pageYOffset;
+
+      // Toggle shadow based on scroll position
+      if (currentScrollPosition === 0) {
+        setIsAtTop(true); // We're at the top of the page
+      } else {
+        setIsAtTop(false); // We're scrolling away from the top
+      }
+
+      if (currentScrollPosition > scrollPosition && !isAnimating) {
+        // Scrolling down, hide navbar
+        setIsAnimating(true);
+        gsap.to(".navbar", {
+          y: -100, // Move navbar off-screen
+          duration: 0.5,
+          ease: "power2.out",
+          onComplete: () => setIsAnimating(false),
+        });
+      } else if (currentScrollPosition < scrollPosition && !isAnimating) {
+        // Scrolling up, show navbar
+        setIsAnimating(true);
+        gsap.to(".navbar", {
+          y: 0, // Bring navbar back to the top position
+          duration: 0.5,
+          ease: "power2.out",
+          onComplete: () => setIsAnimating(false),
+        });
+      }
+
+      setScrollPosition(currentScrollPosition);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollPosition, isAnimating]);
+
   return (
     <Box
-      className="roboto"
+      className="navbar"
       sx={{
         width: "100%",
         display: "flex",
@@ -29,6 +74,10 @@ function Navbar() {
         height: "85px",
         alignItems: "center",
         position: "fixed",
+        fontFamily: '"Roboto Mono", monospace',
+        backdropFilter: " blur(20px)",
+        boxShadow: !isAtTop && "0 4px 10px rgba(0, 0, 0, 0.1)",
+        zIndex: "1000",
       }}
     >
       <img
