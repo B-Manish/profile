bc06e2c5daaaf0c9d21016e34d4828c9f5c5b5ad
batchumanish <batchumanish@gmail.com>
2024-10-22 14:24:21 +0530

    Changed threed model rotation

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,5 +1,4 @@
-import React, { useState, useRef, Suspense } from "react";
-import { Box } from "@mui/material";
+import React, { useState, useEffect, Suspense } from "react";
 import "../App.css";
 import {
   Html,
@@ -8,8 +7,6 @@ import {
   View,
 } from "@react-three/drei";
 import Lights from "./Lights";
-import Laptop from "./Laptop";
-import Laptoptwo from "./Laptoptwo";
 import Iphone from "./Iphone";
 import * as THREE from "three";
 
@@ -24,6 +21,25 @@ const Test = ({
   height = "calc(80vh - 150px)",
   width = "450px",
 }) => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  // Handle mouse movement and update mouse position state
+  const handleMouseMove = (event) => {
+    const { clientX, clientY } = event;
+    setMousePosition({
+      x: (clientX / window.innerWidth) * 2 - 1,
+      y: -(clientY / window.innerHeight) * 2 + 1,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
     <View
       index={index}
@@ -35,13 +51,14 @@ const Test = ({
     >
       {/* Ambient Light */}
       <ambientLight intensity={0.3} />
-      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      <PerspectiveCamera makeDefault position={[0, 0, 5]} />
       <Lights />
       <OrbitControls
         makeDefault
         ref={controlRef}
         enableZoom={false}
-        enablePan={0.4}
+        enablePan={false} // Disable panning
+        enableRotate={false} // Disable default rotation
         rotateSpeed={0.1}
         target={new THREE.Vector3(0, 0, 0)}
         onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
@@ -50,6 +67,7 @@ const Test = ({
         ref={groupRef}
         name={`${index === 1}?"small":"large"`}
         position={[0, -1.4, 0]}
+        rotation={[mousePosition.y * 0.05, mousePosition.x * 0.05, 0]} // Controls rotation intensity
       >
         <Suspense
         // fallback={
