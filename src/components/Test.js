5a1a70673d81b7537c6098e2aa898d8946c64709
batchumanish <batchumanish@gmail.com>
2024-10-22 14:45:35 +0530

    Added CustomRotationControls

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, Suspense } from "react";
-import "../App.css";
+import React, { useState, useRef, useEffect, Suspense } from "react";
+import { useFrame } from "@react-three/fiber";
 import {
   Html,
   OrbitControls,
@@ -20,8 +20,10 @@ const Test = ({
   size,
   height = "calc(80vh - 150px)",
   width = "450px",
+  rotationSpeed = "0.1",
 }) => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const targetRotation = useRef(new THREE.Euler(0, 0, 0)); // Store target rotation
 
   // Handle mouse movement and update mouse position state
   const handleMouseMove = (event) => {
@@ -40,6 +42,26 @@ const Test = ({
     };
   }, []);
 
+  function CustomRotationControls() {
+    useFrame(() => {
+      // Calculate target rotation based on mouse position
+      targetRotation.current.x = mousePosition.y * 0.05;
+      targetRotation.current.y = mousePosition.x * 0.05;
+
+      // Smoothly interpolate the current rotation towards the target rotation
+      groupRef.current.rotation.x = THREE.MathUtils.lerp(
+        groupRef.current.rotation.x,
+        targetRotation.current.x,
+        rotationSpeed // Adjust lerp factor for smoother/slower or faster transition
+      );
+      groupRef.current.rotation.y = THREE.MathUtils.lerp(
+        groupRef.current.rotation.y,
+        targetRotation.current.y,
+        rotationSpeed
+      );
+    });
+  }
+
   return (
     <View
       index={index}
@@ -49,6 +71,7 @@ const Test = ({
         width: width,
       }}
     >
+      <CustomRotationControls />
       {/* Ambient Light */}
       <ambientLight intensity={0.3} />
       <PerspectiveCamera makeDefault position={[0, 0, 5]} />
@@ -67,7 +90,7 @@ const Test = ({
         ref={groupRef}
         name={`${index === 1}?"small":"large"`}
         position={[0, -1.4, 0]}
-        rotation={[mousePosition.y * 0.05, mousePosition.x * 0.05, 0]} // Controls rotation intensity
+        // rotation={[mousePosition.y * 0.05, mousePosition.x * 0.05, 0]} // Controls rotation intensity
       >
         <Suspense
         // fallback={
